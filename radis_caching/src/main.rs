5387dc3096f349b4f72951ae97f09b76192800fc
Ronnie-Ahmed <rksraisul@gmail.com>
2026-09-17 19:38:32 +0600

    Cached Get product by id

--- radis_caching/src/main.rs
+++ radis_caching/src/main.rs
@@ -13,7 +13,7 @@ use axum::{
 use axum::routing::delete;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Clone, sqlx::FromRow)]
+#[derive(Debug, Deserialize,Serialize, Clone, sqlx::FromRow)]
 pub struct Product {
     id: i32,
     content: String,
@@ -61,7 +61,7 @@ async fn main() {
     let state=AppState{db:pool,redis:redis_conn};
 
     let app = Router::new().route("/home", get(test)).route("/product", post(create_product).get(list_product))
-    .route("/product/{id}", get(get_product_by_id).put(update_product).delete(delete_rpoduct)).with_state(db);
+    .route("/product/{id}", get(get_product_by_id).put(update_product).delete(delete_rpoduct)).with_state(state);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3003").await.unwrap();
     println!("Server Listening on port : http://0.0.0.0:3003");
     axum::serve(listener, app).await.unwrap();
@@ -74,29 +74,46 @@ async fn test() -> Result<String, String> {
 
  //-------------------------------------------------Cache
 
- async fn cache_set<T:Serialize>(
-    con:&mut redis::aio::MultiplexedConnection,
-    key:&str,
-    value:&T,
-    ttl_seconds:u64,
- )-> Result<(),redis::RedisError>{
-    let json=serde_json::to_string(value).expect("Seralization failed");
+async fn cache_set<T: Serialize>(
+    con: &mut redis::aio::MultiplexedConnection,
+    key: &str,
+    value: &T,
+    ttl_seconds: u64,
+) -> Result<(), redis::RedisError> {
+    let json = serde_json::to_string(value)
+        .map_err(|e| {
+            redis::RedisError::from((
+                redis::ErrorKind::TypeError,
+                "JSON serialization failed",
+                e.to_string(),
+            ))
+        })?;
 
-    let _:()=con.set_ex(key, json, ttl_seconds).await?;
+    let _: () = con.set_ex(key, json, ttl_seconds).await?;
+
     Ok(())
- }
-
+}
 
 async fn cache_get<T: for<'a> Deserialize<'a>>(
     con: &mut redis::aio::MultiplexedConnection,
     key: &str,
 ) -> Result<Option<T>, redis::RedisError> {
     let result: Option<String> = con.get(key).await?;
+
     match result {
         Some(json) => {
-            let data: T = serde_json::from_str(&json).expect("Deserialization failed");
+            let data: T = serde_json::from_str(&json)
+                .map_err(|e| {
+                    redis::RedisError::from((
+                        redis::ErrorKind::TypeError,
+                        "JSON deserialization failed",
+                        e.to_string(),
+                    ))
+                })?;
+
             Ok(Some(data))
         }
+
         None => Ok(None),
     }
 }
@@ -147,21 +164,59 @@ async fn update_product(
 }
 
 async fn get_product_by_id(
-    State(db): State<Db>,
+    State(state): State<AppState>,
     Path(id): Path<i32>,
 ) -> Result<Json<Product>, StatusCode> {
+
+    let cache_key=format!("product:{}",id);
+    let mut redis=state.redis.clone();
+
+ // --------------------------------------------------
+    // 1. Check Redis cache
+    // --------------------------------------------------
+
+    match cache_get::<Product>(&mut redis,&cache_key).await{
+        Ok(Some(product))=>{
+            println!("CACHE HIT: {}",cache_key);
+            return Ok(Json(product));
+        }
+        Ok(None)=>{
+            println!("CACHE MISS: {}",cache_key);
+        }
+        Err(error)=>{
+            eprintln!("Redis GET error: {}",error);
+        }
+    }
+    // --------------------------------------------------
+    // 2. Cache miss -> Query PostgreSQL
+    // --------------------------------------------------
+
     let get_product = sqlx::query_as::<_, Product>(
         "
     SELECT id,content,is_sold FROM product WHERE id=$1",
     )
     .bind(id)
-    .fetch_optional(&db)
+    .fetch_optional(&state.db)
     .await
     .unwrap();
-    match get_product {
-        Some(productt) => Ok(Json(productt)),
-        None => Err(StatusCode::NOT_FOUND),
+    if let Some(product) = get_product.clone()  {
+        cache_set(&mut redis, &cache_key, &product, 60*5).await;
+        
     }
+
+
+   let product= match get_product {
+        Some(productt) => productt,
+        None =>return Err(StatusCode::NOT_FOUND),
+    };
+
+    if let Err(error) = cache_set(&mut redis, &cache_key, &product, 5*30).await{
+        eprintln!("Redis SET error : {}",error);
+    }
+
+    
+
+    Ok(Json(product))
 }
 
 async fn delete_rpoduct(State(db): State<Db>,Path(id): Path<i32>)->Result<StatusCode,StatusCode>{
