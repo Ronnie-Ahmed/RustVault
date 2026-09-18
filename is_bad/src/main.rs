3aabec6e6a6a5f2ecb756ef3e391e37c357acf7d
Ronnie-Ahmed <rksraisul@gmail.com>
2026-09-18 11:56:59 +0600

    Is Bad

---
+++ is_bad/src/main.rs
@@ -0,0 +1,24 @@
+
+
+
+// impl Solution {
+
+//     pub fn isBadVersion(version:i32)->bool{
+//         return false
+//     }
+
+
+//     pub fn first_bad_version(&self, n: i32) -> i32 {
+// 		for i in i..=n{
+
+//         }
+//     }
+// }
+
+fn main() {
+    println!("Hello, world!");
+
+    for i in 1..=10{
+        println!("{}",i);
+    }
+}
